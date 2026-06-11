9b916d3ad6fdf1a9ab7e6a0defc689da043c6e86
Soobin <114851926+soobinH@users.noreply.github.com>
2026-06-11 16:04:56 +0900

    Time: 16 ms (23.08%), Space: 19.4 MB (75.25%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen_s = {}
+        seen_t = {}
+        for i, char in enumerate(s):
+            if char in seen_s:
+                seen_s[char] += 1
+            
+            else:
+                seen_s[char] = 1
+            
+        
+        
+        for i, char in enumerate(t):
+            if char in seen_t:
+                seen_t[char] += 1
+            
+            else:
+                seen_t[char] = 1
+
+        
+        if seen_t == seen_s:
+            return True
+        else: return False
